f487df4df9c54f467f349c5a49f16776dd8ef8a7
lwylx0131 <971088726@qq.com>
2019-12-22 10:23:23 +0800

    kNN近临算法

---
+++ 02.机器学习实战/02.k-近邻算法/codes/kNN.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def createDataSet():
+    group = np.array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+dataSet, labels = createDataSet()
+print(dataSet)
