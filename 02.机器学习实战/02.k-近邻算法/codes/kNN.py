a67e494a0d4d65d0fb92f77c0ca83842a6b51974
lwylx0131 <971088726@qq.com>
2019-12-26 02:11:28 +0800

    kNN手写数字识别

--- 02.机器学习实战/02.k-近邻算法/codes/kNN.py
+++ 02.机器学习实战/02.k-近邻算法/codes/kNN.py
@@ -128,7 +128,44 @@ testVector = img2vector('digits/testDigits/0_13.txt')
 def handwritingClassTest():
     trainingFileList = os.listdir('digits/trainingDigits')
     m = len(trainingFileList)
-    for i in trainingFileList:
-        print(i)
+    hwLabels = []
+    # 初始化1024列的矩阵，因为一个数字文件有32行32列
+    trainingMatrix = np.zeros((m, 32 * 32))
+    for i in range(m):
+        # 文件名格式为9_102.txt
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        # 从文件名中解析出数字标签9
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        # 将每个文件的内容解析为一行放入到trainingMatrix矩阵中作为训练数据
+        trainingMatrix[i, :] = img2vector('digits/trainingDigits/%s' % fileNameStr)
+    testFileList = os.listdir('digits/testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        # 从文件名中解析出数字标签真实值
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('digits/testDigits/%s' % fileNameStr)
+        # 通过分类器判别出的数字标签值
+        classifierResult = classify0(vectorUnderTest, trainingMatrix, hwLabels, 3)
+        print('[%d]-数字图像的真实标签值: %d, 分类器判断的数字图像标签值: %d' %(i, classNumStr, classifierResult))
+        if(classifierResult != classNumStr):
+            errorCount += 1.0
+    print('分类错误数: %d, 分类错误率: %f' %(errorCount, errorCount / float(mTest)))
 
 handwritingClassTest()
+'''
+实际使用这个算法时，算法的执行效率并不高。因为算法需要为每个测试向量做2000次距离计算，
+每个距离计算包括了1024个维度浮点运算，总计要执行900次，此外，我们还需要为测试向量准备2 M B的存储空间。
+是否存在一种算法减少存储空间和计算时间的开销呢？ k决策树就是A-近邻算法的优化版，可以节省大量的计算开销。
+
+k-近邻算法是分类数据最简单最有效的算法，本章通过两个例子讲述了如何使用k-近邻算法构造分类器。
+k-近邻算法是基于实例的学习，使用算法时我们必须有接近实际数据的训练样本数据。
+k-近邻算法必须保存全部数据集，如果训练数据集的很大，必须使用大量的存储空间。
+此外,由于必须对数据集中的每个数据计算距离值，实际使用时可能非常耗时。
+k-近邻算法的另一个缺陷是它无法给出任何数据的基础结构信息，因此我们也无法知晓平均实例样本和典型实例样本具有什么特征。
+下一章我们将使用概率测量方法处理分类问题，该算法可以解决这个问题
+'''
