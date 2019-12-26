862e3170341ccfa70c880c01cecf7c664cd2aa89
lwylx0131 <971088726@qq.com>
2019-12-26 11:31:31 +0100

    决策树-计算香农熵

--- 02.机器学习实战/03.决策树/codes/treeID3.py
+++ 02.机器学习实战/03.决策树/codes/treeID3.py
@@ -12,6 +12,35 @@ p(xi): 表示选择该分类的概率
 熵的公式：
 H = -[求和符号]p(xi) * log2p(xi)
 i: 表示1~n，表示分类的数目
+'''
+import math
 
 '''
+1)计算给定数据的香农熵;
+'''
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        # 以字典的形式统计标签类的数量: {'yes': 10, 'no': 20} 
+        labelCounts[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCounts:
+        # 使用所有类标签的发生频率来计算类别出现的概率
+        prob = float(labelCounts[key]) / numEntries
+        # 使用香农熵的公式进行计算
+        shannonEnt -= prob * math.log(prob, 2)
+    return shannonEnt
+
+def createDataSet():
+    dataSet = [[1,1,'yes'], [1,1,'yes'], [1,0,'no'], [0,1,'no'], [0,1,'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+dataSet, labels = createDataSet()
+print(calcShannonEnt(dataSet)) # 0.9709505944546686
+        
 
