ed454642322cc3fba91850f664e278f67458cd8a
lwylx0131 <971088726@qq.com>
2020-01-02 00:43:45 +0800

    Logistic回归-绘制决策边界线

--- 02.机器学习实战/05.Logistic回归/codes/logRegres.py
+++ 02.机器学习实战/05.Logistic回归/codes/logRegres.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 import numpy as np
+import matplotlib.pyplot as plt
 
 def loadDataSet():
     # 训练数据集100*3的特征矩阵
@@ -37,3 +38,39 @@ def gradAscent(dataMatrix, labelMatrix):
 dataMatrix, labelMatrix = loadDataSet()
 weights = gradAscent(dataMatrix, labelMatrix)
 print('Logistic回归梯度上升最优算法模型值:\n{}'.format(weights))
+
+def plotBestFit(weights):
+    dataMatrix, labelMatrix = loadDataSet()
+    # list转换为np.ndarray
+    dataArr = np.array(dataMatrix)
+    n = np.shape(dataArr)[0]
+    # 分类标签为1时，将所有的x1放入到xcord1数组，所有的x2放入到ycord1数组
+    # 分类标签为0时，将所有的x1放入到xcord2数组，所有的x2放入到ycord2数组
+    # 注意dataMatrix第一列w0为默认1，即x0对应的模型参数值
+    xcord1 = []
+    ycord1 = []
+    xcord2 = []
+    ycord2 = []
+    for i in range(n):
+        if int(labelMatrix[i]) == 1:
+            xcord1.append(dataArr[i, 1])
+            ycord1.append(dataArr[i, 2])
+        else:
+            xcord2.append(dataArr[i, 1])
+            ycord2.append(dataArr[i, 2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    # 将数据集x1、x2显示在坐标平面
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    # 60条数据记录
+    x = np.arange(-3.0, 3.0, 0.1) 
+    # 因为sigmoid函数在0处(0,0.5)是分类标签1/0的分界线，所以需要sigmoid函数的-WX=0
+    # 即w0*x0+w1*x1+w2*x2=0，所以推出以下y的公式，即求x2=(-w0-w1*x1)/w2
+    y = np.array(((-weights[0]-weights[1]*x) / weights[2]).transpose())
+    ax.plot(x, y)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
+    
+plotBestFit(weights)
