6dc0fcbb70925ba697d44109dd2b757113b33537
lwylx0131 <971088726@qq.com>
2020-01-02 11:37:17 +0100

    Logistic回归-改进随机梯度上升算法

--- 02.机器学习实战/05.Logistic回归/codes/logRegres.py
+++ 02.机器学习实战/05.Logistic回归/codes/logRegres.py
@@ -1,6 +1,7 @@
 # -*- encoding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 def loadDataSet():
     # 训练数据集100*3的特征矩阵
@@ -73,4 +74,53 @@ def plotBestFit(weights):
     plt.ylabel('X2')
     plt.show()
     
+#plotBestFit(weights)
+
+'''
+梯度上升算法：
+  在每次更新回归系数时都需要遍历整个数据集，如果只是处理100个左右数据尚可，但如果处理数十亿样本和成千上万的特征，那计算复杂度太高。
+改进方法-随机梯度上升算法：
+  一次仅使用一个样本点来更新回归系数，由于可以在新样本到来时对分类器进行增量式更新，因此随机梯度上升算法是一个在线学习算法。
+'''
+def stocGradAscent0(dataMatrix, classLabels):
+    m, n = np.shape(dataMatrix) # (100, 3)
+    alpha = 0.01
+    weights = np.ones(n) # (3,1)
+    for i in range(m):
+        # 单个样本点 wx=w0x0+w1x1+w2x2=(1,3)*(3*1)=(1,1)
+        h = sigmoid(dataMatrix[i] * weights)
+        error = classLabels[i] - h # (1, 1)
+        # 更新模型参数 (3,1)+(1,)*(1,)*(3,1)=(3,1)
+        weights = weights + alpha * error * dataMatrix[i]
+    return weights
+
+dataMatrix, labelMatrix = loadDataSet()
+weights = stocGradAscent0(dataMatrix, labelMatrix)
+print('Logistic回归随机梯度上升最优算法模型值:\n{}'.format(weights))
+#plotBestFit(weights)
+
+'''
+以上随机梯度上升算法中，根据X0/X1/X2的迭代次数关系图（没画），在大的波动停止后，还有一些小的周期性波动。
+产生这种现象的原因是存在一些不能正确分类的样本点（数据集并非线性可分），在每次迭代时会引发系数的剧烈改变。
+我们期望算法能够避免来回波动，从而收敛到某个值。另外，收敛速度也需要加快，可通过以下算法改进。
+'''
+def stocGradAscent1(dataMatrix, classLabels, numIter=150):
+    m, n = np.shape(dataMatrix)
+    weights = np.ones(n)
+    for j in range(numIter):
+        # 临时记录数据数量，当随机获取一条计算之后就删除该条记录
+        dataIndex = list(range(m))
+        for i in range(m):
+            alpha = 4 / (1.0 + j + i) + 0.01
+            # 随机从数据集中获取数据记录
+            randIndex = int(random.uniform(0, len(dataIndex)))
+            h = sigmoid(dataMatrix[randIndex] * weights)
+            error = classLabels[randIndex] - h
+            weights = weights + alpha * error * dataMatrix[randIndex]
+            del[dataIndex[randIndex]]
+    return weights
+
+dataMatrix, labelMatrix = loadDataSet()
+weights = stocGradAscent1(dataMatrix, labelMatrix, 500)
+print('Logistic回归alpha变动随机梯度上升最优算法模型值:\n{}'.format(weights))
 plotBestFit(weights)
